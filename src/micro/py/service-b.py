0f6fa07022cfa5ee90c5894db66de051e7b1cf8e
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2020-01-07 18:12:06 +0100

    simple services added

---
+++ src/micro/py/service-b.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/bye')
+def hello():
+    return {"type": "spock"}, 200
+
+def get_nomad_assigned_port():
+    return os.environ.get('NOMAD_PORT_port_server', 5001)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=get_nomad_assigned_port())
